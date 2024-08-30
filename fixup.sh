fd00cba6527c00f92d34d49064e2b53e6d84d8b8
yunshin <2younsin@gmail.com>
2024-08-30 23:26:05 +0900

    fix: entry point 재설정

--- fixup.sh
+++ fixup.sh
@@ -1,14 +1,10 @@
-cp dist/mjs/index.d.ts dist 
-
-rm -rf dist/*/index.d.ts 
-
 cat >dist/cjs/package.json <<!EOF
 {
     "type": "commonjs"
 }
 !EOF
 
-cat >dist/mjs/package.json <<!EOF
+cat >dist/esm/package.json <<!EOF
 {
     "type": "module"
 }
