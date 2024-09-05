7a09fd945ba7b0fc2b2f17d13a5dbe3080a2d775
yunshin <2younsin@gmail.com>
2024-09-05 15:51:30 +0900

    fix: entry point 경로 오류 수정

--- fixup.sh
+++ fixup.sh
@@ -1,10 +1,10 @@
-cp dist/mjs/index.js dist/mjs/index.mjs
+cp dist/esm/index.js dist/esm/index.mjs
 cp dist/cjs/index.js dist/cjs/index.cjs
 
-rm -rf dist/mjs/index.js dist/cjs/index.js
+rm -rf dist/esm/index.js dist/cjs/index.js
 
 
-cp dist/mjs/index.d.ts dist/mjs/index.d.mts
+cp dist/esm/index.d.ts dist/esm/index.d.mts
 cp dist/cjs/index.d.ts dist/cjs/index.d.cts
 
-rm -rf dist/mjs/index.d.ts dist/cjs/index.d.ts
+rm -rf dist/esm/index.d.ts dist/cjs/index.d.ts
