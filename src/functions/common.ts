dae58ebd719cf15833e1dd3efcfabb22d8a73142
yunshin <2younsin@gmail.com>
2024-09-05 15:43:46 +0900

    refactor: 일반함수 분리

---
+++ src/functions/common.ts
@@ -0,0 +1,49 @@
+export const isEqual = <T>(target: T, to: T) => Object.is(target, to)
+export const isLess = (target: number, than: number) => target < than
+export const isMore = (target: number, than: number) => target > than
+export const isEqualOrLess = (target: number, than: number) => target <= than
+export const isEqualOrMore = (target: number, than: number) => target >= than
+
+export const isPositive = (target: number) => target > 0
+export const isNegative = (target: number) => target < 0
+
+export const clamp = (value: number, max: number, min: number) =>
+  Math.min(Math.max(value, min), max)
+
+export const generateRandomInteger = (max: number = 1, min: number = 0) =>
+  Math.floor(Math.random() * (max - min + 1)) + min
+
+export const normalize = (value: number, max: number, min: number = 0) => {
+  if (max <= min)
+    throw new Error('Maximum value must be greater than minimum value.')
+  const result = (value - min) / (max - min)
+  return result
+}
+
+export const getMinimum = (values: number[]): number => {
+  if (values.length === 0) throw new Error('Array cannot be empty.')
+
+  let minimum = Number.MAX_SAFE_INTEGER
+  values.forEach((value) => {
+    if (minimum > value) minimum = value
+  })
+  return minimum
+}
+
+export const getMaximum = (values: number[]): number => {
+  if (values.length === 0) throw new Error('Array cannot be empty.')
+
+  let maximum = Number.MIN_SAFE_INTEGER
+  values.forEach((value) => {
+    if (maximum < value) maximum = value
+  })
+  return maximum
+}
+
+export const removeSpecialCharacters = (str: string) =>
+  str.replace(/[^a-zA-Z0-9\s]/g, '')
+
+export const shallowCopy = <T extends object>(original: T) => {
+  if (Array.isArray(original)) return original.slice(0) as T
+  return Object.assign({}, original)
+}
--- src/functions/util.ts
+++ src/functions/util.ts
@@ -1,134 +1,188 @@
-import { HexCode, HexCode6, HexCode8, HSLA, RGBA } from '..'
-import { isHexCode6, isHexCode8 } from './type-guard'
+import { CodeFormat, HexCode, HSLA, RGBA } from '..'
+import {
+  clamp,
+  generateRandomInteger,
+  getMaximum,
+  getMinimum,
+  isEqual,
+  isEqualOrMore,
+  isLess,
+  isMore,
+  isNegative,
+  normalize,
+} from './common.js'
+import { isHexCode, isHexCode6 } from './type-guard.js'
 
-export const generateRandomHexCode8 = (): HexCode8 => {
-  const randomInt = generateRandomInteger(0xffffff).toString()
-  const hexCode6 = ['#', randomInt.padStart(6, '0')].join() as HexCode6
-  const hexCode8 = convertHexCode6To8(hexCode6, 'FF')
-  return hexCode8
+export const generateRGBA = (
+  r: number,
+  g: number,
+  b: number,
+  a: number
+): RGBA => {
+  if (isLess(r, 0) && isMore(r, 255)) throw new Error()
+  if (isLess(g, 0) && isMore(g, 255)) throw new Error()
+  if (isLess(b, 0) && isMore(b, 255)) throw new Error()
+  if (isLess(a, 0) && isMore(a, 1)) throw new Error()
+
+  const rgba: RGBA = { r, g, b, a }
+  return rgba
 }
 
-export const convertHexCode8To6 = (hexCode8: HexCode8): HexCode6 => {
-  if (!isHexCode8(hexCode8)) throw new Error('Invalid hexCode format provided.')
-  const hexCode6 = hexCode8.slice(0, -2)
-  return hexCode6 as HexCode6
+export const generateHSLA = (
+  h: number,
+  s: number,
+  l: number,
+  a: number
+): HSLA => {
+  if (isLess(h, 0) && isMore(h, 360)) throw new Error()
+  if (isLess(s, 0) && isMore(s, 100)) throw new Error()
+  if (isLess(l, 0) && isMore(l, 100)) throw new Error()
+  if (isLess(a, 0) && isMore(a, 1)) throw new Error()
+
+  const hsla: HSLA = { h, s, l, a }
+  return hsla
 }
 
-export const convertHexCode6To8 = (
-  hexCode6: HexCode6,
-  alpha: string
-): HexCode8 => {
-  if (!isHexCode6(hexCode6)) throw new Error('Invalid hexCode format provided.')
-  const hexCode8 = [hexCode6, alpha].join() as HexCode8
-  return hexCode8
+export const initRGBAWithBlack = (): RGBA => {
+  const rgba = generateRGBA(0, 0, 0, 1)
+  return rgba
+}
+
+export const generateRandomRGBA = (): RGBA => {
+  const rgba = generateRGBA(
+    generateRandomInteger(255),
+    generateRandomInteger(255),
+    generateRandomInteger(255),
+    1
+  )
+  return rgba
 }
 
 export const convertHexToRGBA = (hexCode: HexCode): RGBA => {
-  if (!isHexCode8(hexCode)) throw new Error('Invalid hexCode format provided.')
+  if (!isHexCode(hexCode)) throw new Error('Invalid hexCode format provided.')
 
   const noSign = hexCode.slice(1)
   const r = parseInt(noSign.slice(0, 2), 16)
   const g = parseInt(noSign.slice(2, 4), 16)
   const b = parseInt(noSign.slice(4, 6), 16)
-  if (isHexCode6(hexCode)) return { r, g, b, a: 1 }
-
-  const alphaValue = parseInt(noSign.slice(6), 16)
-  const normalizedAlpha = normalize(alphaValue, 255)
-  return { r, g, b, a: normalizedAlpha }
-}
-
-export const convertRGBAToHex = (rgba: RGBA): HexCode8 => {
-  const r = clamp(rgba.r, 0, 255)
-  const g = clamp(rgba.g, 0, 255)
-  const b = clamp(rgba.b, 0, 255)
-  const alphaValue = clamp(Math.round(rgba.a * 255), 0, 255)
-
-  const hexCode8 = `#${toHex(r)}${toHex(g)}${toHex(b)}${toHex(alphaValue)}`
-  return hexCode8 as HexCode8
-}
-
-export const convertRGBAToHSLA = ({ r, g, b, a }: RGBA): HSLA => {
-  r = normalize(r, 255)
-  g = normalize(g, 255)
-  b = normalize(b, 255)
-
-  const max = Math.max(r, g, b)
-  const min = Math.min(r, g, b)
-
-  const h =
-    max === min
-      ? 0
-      : (max === r
-          ? (g - b) / (max - min)
-          : max === g
-            ? (b - r) / (max - min) + 2
-            : (r - g) / (max - min) + 4) * 60
-  const l = (max + min) / 2
-  const s = max === min ? 0 : (max - min) / (1 - Math.abs(2 * l - 1))
-
-  return { h, l, s, a }
-}
-
-export const convertHSLAToRGBA = ({ h, s, l, a }: HSLA): RGBA => {
-  const HueToRGB = (p: number, q: number, t: number) => {
-    if (isLess(t, 0)) t += 1
-    if (isMore(t, 1)) t -= 1
-
-    if (isLess(t, 1 / 6)) return p + (q - p) * 6 * t
-    if (isLess(t, 1 / 2)) return q
-    if (isLess(t, 2 / 3)) return p + (q - p) * (2 / 3 - t) * 6
-    return p
+  if (isHexCode6(hexCode)) {
+    const rgba = generateRGBA(r, g, b, 1)
+    return rgba
   }
 
-  if (s === 0) {
-    const r = Math.round(l * 255)
-    const g = Math.round(l * 255)
-    const b = Math.round(l * 255)
-    return { r, g, b, a }
-  }
-
-  const q = isLess(l, 0.5) ? l * (1 + s) : l + s - l * s
-  const p = 2 * l - q
-
-  const rFloat = HueToRGB(p, q, h + 1 / 3)
-  const gFloat = HueToRGB(p, q, h)
-  const bFloat = HueToRGB(p, q, h - 1 / 3)
-
-  const [r, g, b] = [rFloat, gFloat, bFloat].map((float) => Math.round(float))
-
-  return { r, g, b, a }
+  let a = parseInt(noSign.slice(6), 16)
+  a = normalize(a, 255)
+  const rgba = generateRGBA(r, g, b, a)
+  return rgba
 }
 
-/* --------[Common]-------- */
+export const convertRGBAToHex = (
+  rgba: RGBA,
+  codeFormat: CodeFormat
+): HexCode => {
+  const { r, g, b, a } = rgba
+
+  if (isEqual(codeFormat, 'hexCode6'))
+    return `#${toHex(r)}${toHex(g)}${toHex(b)}`
+
+  const clampedA = clamp(Math.round(a * 255), 0, 255)
+  return `#${toHex(r)}${toHex(g)}${toHex(b)}${toHex(clampedA)}`
+}
+
+export const convertRGBAToHSLA = (rgba: RGBA): HSLA => {
+  const h = getHueFromRGBA(rgba)
+  const s = getSaturationFromRGBA(rgba)
+  const l = getLightnessFromRGBA(rgba)
+  const { a } = rgba
+  const hsla = generateHSLA(h, s, l, a)
+  return hsla
+}
+
+export const getHueFromRGBA = (rgba: RGBA) => {
+  const { r, g, b } = rgba
+  const max = getMaximum([r, g, b])
+  const min = getMinimum([r, g, b])
+  const delta = max - min
+
+  let h = 0
+  if (isEqual(max, min)) h = 0
+  else if (isEqual(max, r)) h = ((g - b) / delta) % 6
+  else if (isEqual(max, g)) h = (b - r) / delta + 2
+  else h = (r - g) / delta + 4
+
+  h = h * 60
+  if (isNegative(h)) h += 360
+
+  return h
+}
+
+export const getSaturationFromRGBA = (rgba: RGBA) => {
+  const { r, g, b } = rgba
+  const max = getMaximum([r, g, b])
+  const min = getMinimum([r, g, b])
+
+  const delta = max - min
+
+  if (isEqual(max, min)) return 0
+
+  const l = getLightnessFromRGBA(rgba)
+  const s = delta / (1 - Math.abs(2 * l - 1))
+  return s
+}
+
+export const getLightnessFromRGBA = (rgba: RGBA) => {
+  const { r, g, b } = rgba
+  const max = getMaximum([r, g, b])
+  const min = getMinimum([r, g, b])
+  const lightness = (max + min) / 2
+  return lightness
+}
+
+export const convertHSLAToRGBA = (hsla: HSLA): RGBA => {
+  const { h, s, l, a } = hsla
+  const normalizedS = normalize(s, 100)
+  const normalizedL = normalize(l, 100)
+
+  const c = (1 - Math.abs(2 * normalizedL - 1)) * normalizedS
+  const x = c * (1 - Math.abs(((h / 60) % 2) - 1))
+  const m = normalizedL - c / 2
+
+  let r = 0,
+    g = 0,
+    b = 0
+
+  if (isEqualOrMore(h, 0) && isLess(h, 60)) {
+    r = c
+    g = x
+    b = 0
+  } else if (isEqualOrMore(h, 60) && isLess(h, 120)) {
+    r = x
+    g = c
+    b = 0
+  } else if (isEqualOrMore(h, 120) && isLess(h, 180)) {
+    r = 0
+    g = c
+    b = x
+  } else if (isEqualOrMore(h, 180) && isLess(h, 240)) {
+    r = 0
+    g = x
+    b = c
+  } else if (isEqualOrMore(h, 240) && isLess(h, 300)) {
+    r = x
+    g = 0
+    b = c
+  } else if (isEqualOrMore(h, 300) && isLess(h, 360)) {
+    r = c
+    g = 0
+    b = x
+  }
+
+  r = Math.round((r + m) * 255)
+  g = Math.round((g + m) * 255)
+  b = Math.round((b + m) * 255)
+
+  const rgba = generateRGBA(r, g, b, a)
+  return rgba
+}
 
 export const toHex = (value: number) => value.toString(16).padStart(2, '0')
-
-export const isEqual = <T>(target: T, to: T) => Object.is(target, to)
-export const isLess = (target: number, than: number) => target < than
-export const isMore = (target: number, than: number) => target > than
-
-export const clamp = (value: number, max: number, min: number) =>
-  Math.min(Math.max(value, min), max)
-
-export const generateRandomInteger = (max: number = 1, min: number = 0) =>
-  Math.floor(Math.random() * (max - min + 1)) + min
-
-export const normalize = (value: number, max: number, min: number = 0) => {
-  if (max <= min)
-    throw new Error('Maximum value must be greater than minimum value.')
-  const delta = max - min
-  return (value - min) / delta
-}
-
-export const getMinimum = (values: number[]): number => {
-  if (values.length === 0) throw new Error('Array cannot be empty.')
-
-  let minimum = Number.MAX_SAFE_INTEGER
-  values.forEach((value) => {
-    if (minimum > value) minimum = value
-  })
-  return minimum
-}
-
-export const removeSpecialCharacters = (str: string) =>
-  str.replace(/[^a-zA-Z0-9\s]/g, '')
