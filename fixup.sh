0ded176e78e4dd6d0739cae42f8b3a300cc0b15b
yunshin <2younsin@gmail.com>
2024-09-10 02:49:31 +0900

    build: entry-point 수정, 모듈별 package.json 작성

--- fixup.sh
+++ fixup.sh
@@ -1,13 +1,2 @@
-cp dist/esm/index.js dist/esm/index.mjs
-cp dist/cjs/index.js dist/cjs/index.cjs
-
-rm -rf dist/esm/index.js dist/cjs/index.js
-
-
-cp dist/esm/index.d.ts dist/esm/index.d.mts
-cp dist/cjs/index.d.ts dist/cjs/index.d.cts
-
-rm -rf dist/esm/index.d.ts dist/cjs/index.d.ts
-
-
 echo '{"type": "commonjs"}' > dist/cjs/package.json
+echo '{"type": "module"}' > dist/esm/package.json
